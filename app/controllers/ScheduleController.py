93df6932098bb0d32324809e4ab8db432fe89550
Glebka <Glebka@users.noreply.github.com>
2024-04-05 00:06:32 +0300

    WIP. Add flake8 and pylint configuration files, and make formatting improvements in ScheduleController.py

--- app/controllers/ScheduleController.py
+++ app/controllers/ScheduleController.py
@@ -25,12 +25,13 @@ class ScheduleController:
         self.tz = timezone
 
     def aggregate_day_schedule(self, weekday: int, group: str):
-        day_schedule: dict[str,
-                           str] = SCHEDULE[group][str(weekday + 1)]
+        day_schedule: dict[str, str] = SCHEDULE[group][str(weekday + 1)]
         aggregated_day_schedule: dict[str, list[int]] = dict()
 
-        schedule_pairs = sorted([(value, int(key)-1)
-                                 for key, value in day_schedule.items()], key=lambda d: d[1])
+        schedule_pairs = sorted(
+            [(value, int(key) - 1) for key, value in day_schedule.items()],
+            key=lambda d: d[1],
+        )
         aggregated_day_schedule = defaultdict(list)
         helper = defaultdict(list)
 
@@ -41,13 +42,13 @@ class ScheduleController:
             helper[state].append(0 if length == 1 else (array[-1] - array[-2]))
 
         for state, timing in aggregated_day_schedule.items():
-            new_timing = [timing[i] if helper[state][i] != 1 else None
-                          for i in range(len(timing))]
-            aggregated_day_schedule[state] = list(
-                filter(lambda i: i, new_timing))
+            new_timing = [
+                timing[i] if helper[state][i] != 1 else None for i in range(len(timing))
+            ]
+            aggregated_day_schedule[state] = list(filter(lambda i: i, new_timing))
 
         return aggregated_day_schedule
-    
+
     def _next_zone_datetime_by_type(self, group: str, zone_type: str) -> datetime:
         now = datetime.now(self.tz)
         day_of_week = now.weekday()
@@ -60,9 +61,15 @@ class ScheduleController:
         for t in todays_zone_timing:
             if t > hour:
                 return now.replace(hour=t, minute=0, second=0, microsecond=0)
-        return (now + timedelta(hours=24)).replace(hour=tomorrows_zone_timing[0], minute=0, second=0, microsecond=0).astimezone(pytz.utc)
+        return (
+            (now + timedelta(hours=24))
+            .replace(hour=tomorrows_zone_timing[0], minute=0, second=0, microsecond=0)
+            .astimezone(pytz.utc)
+        )
 
-    def get_next_zone_type_and_time(self, group: str) -> Tuple[ElectricityStateLog.STATE, datetime]:
+    def get_next_zone_type_and_time(
+        self, group: str
+    ) -> Tuple[ElectricityStateLog.STATE, datetime]:
         zones = dict(ON="yes", OFF="no", UNKNOWN="maybe")
         current_state = self.get_state(group)
         del zones[current_state]
@@ -77,7 +84,7 @@ class ScheduleController:
 
     def next_black_zone_datetime(self, group: str) -> datetime:
         return self._next_zone_datetime_by_type(group, "no")
-    
+
     def next_gray_zone_datetime(self, group: str) -> datetime:
         return self._next_zone_datetime_by_type(group, "maybe")
 
@@ -86,20 +93,26 @@ class ScheduleController:
 
     def timedelta_to_next_recovery(self, group: str) -> timedelta:
         next_recovery = self.next_white_zone_datetime(group)
-        return next_recovery - datetime.now(pytz.utc) if next_recovery is not None else None
+        return (
+            next_recovery - datetime.now(pytz.utc)
+            if next_recovery is not None
+            else None
+        )
 
     def timedelta_to_next_blackout(self, group: str) -> timedelta:
         next_blackout = self.next_black_zone_datetime(group)
-        return next_blackout - datetime.now(pytz.utc) if next_blackout is not None else None
+        return (
+            next_blackout - datetime.now(pytz.utc)
+            if next_blackout is not None
+            else None
+        )
 
-    
     def get_state(self, group: str) -> ElectricityStateLog.STATE:
         STATE = ElectricityStateLog.STATE
         now = datetime.now(self.tz)
         weekday = now.weekday()
         schedule = SCHEDULE[group][str(weekday + 1)]
-        state_translation = dict(
-            yes=STATE.ON, no=STATE.OFF, maybe=STATE.UNKNOWN)
+        state_translation = dict(yes=STATE.ON, no=STATE.OFF, maybe=STATE.UNKNOWN)
         return state_translation[schedule[str(now.time().hour + 1)]]
 
 
